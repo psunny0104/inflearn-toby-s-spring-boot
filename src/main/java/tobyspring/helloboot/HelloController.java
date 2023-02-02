84e8860368d4528b2c21ff08e6e0e9e10f11c0cd
psunny0104 <pskimdev@gmail.com>
2023-02-02 12:42:17 +0900

    독립 실행형 스프링 애플리케이션: 의존 오브젝트 DI 적용

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,13 +1,15 @@
 package tobyspring.helloboot;
 
 import java.util.Objects;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 public class HelloController {
-    public String hello(String name) {
-        SimpleHelloService helloService = new SimpleHelloService();
+    private final HelloService helloService;
 
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name)); // null 검증
     }
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -25,6 +25,7 @@ public class HellobootApplication {
 	public static void main(String[] args) {
 		GenericApplicationContext applicationContext = new GenericApplicationContext();
 		applicationContext.registerBean(HelloController.class);
+		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
 
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
